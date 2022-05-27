6663f559295e092bc97f8644dd67c372d8e56141
dattranbenit <dattran.benitl@gmail.com>
2022-05-27 16:35:29 +0700

    edit loading logo

--- src/components/Loader/LoaderLogo.js
+++ src/components/Loader/LoaderLogo.js
@@ -3,20 +3,26 @@ import "../header/Header.css";
 import { greeting } from "../../portfolio";
 
 //images
-const loader = require("../../assests/images/animation_640_kzm2m58j.gif")
+const loader = require("../../assests/images/animation_640_kzm2m58j.gif");
 
 class LogoLoader extends React.Component {
   render() {
     const theme = this.props.theme;
     return (
-      <div className="header" style={{display: "flex", flexDirection: "column", marginTop: "-17%"}}>
+      <div
+        className="header"
+        style={{ display: "flex", flexDirection: "column", marginTop: "-17%" }}
+      >
         <div>
-          <img src={loader} alt="Loader" />
+          <img src={loader} alt="Loader" width={"100%"} />
         </div>
-        <div style={{marginTop: "-100px"}}>
+        <div style={{ marginTop: "-100px" }}>
           <span style={{ color: theme.text }}> &lt;</span>
-          <span className="logo-name" style={{ color: theme.text, fontSize: 50, fontWeight: 700 }}>
-                {greeting.logo_name}
+          <span
+            className="logo-name"
+            style={{ color: theme.text, fontSize: 50, fontWeight: 700 }}
+          >
+            {greeting.logo_name}
           </span>
           <span style={{ color: theme.text }}>/&gt;</span>
         </div>
